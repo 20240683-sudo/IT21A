083be730a09fcb360f8ecaf9623b5cb624a35c5c
DenniesBocon <jirehjamesc@gmail.com>
2025-09-05 10:27:26 +0800

    demo for javascript

---
+++ Documents/Luchin & Tudtud Devs/IT21C/index.js
@@ -0,0 +1,46 @@
+/*console.log("hello world");
+
+let student = "ohwen";
+let grade = 1.0;
+let isPassed = true;
+let address;
+let partner = null;
+
+console.log("student name : "+student + "\n Final Grade : "+ grade + "\n Passed : "+ isPassed + "\n Address:"+ address + "\n Partner : "+ partner );
+
+
+let num1 = 10;
+let num2 = 5;
+
+let sum = num1 + num2;
+let diff = num1 - num2;
+let prod = num1 * num1;
+let quo = num1/ num2;
+let mod = num1 % num2;
+
+console.log("Sum : "+ sum + "\n Difference : "+ diff + "\n Product : "+ prod + "\n Quotient : "+ quo + "\n Remainder : "+ mod );
+*/
+let grade = 75;
+
+// conditional statement
+
+if (grade > 75 ) {
+    console.log("student passsed");
+} else {
+    console.log("student failed");
+}
+
+// loop statement
+
+//for (let i = 0; i <= 5; i++) {
+  //  console.log("Count : "+ i);
+//}
+
+// loop statement while
+
+let i = 0;
+
+while (i <= 5) {
+    console.log("Count while : "+ i);
+    i++;
+}
