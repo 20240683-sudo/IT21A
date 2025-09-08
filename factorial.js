3f72abfcab17031071fda1629b6fcf2ac4f9747c
Kim Lucero <20240683@nbsc.edu.ph>
2025-09-08 08:50:35 +0800

    Added factorial

--- factorial.js
+++ factorial.js
@@ -5,6 +5,11 @@ const rl = readline.createInterface({
     output: process.stdout
 })
 
+function mainMenu(){
+    console.log("Factorial Application");
+
+}
+
 console.log("factorial of a number");
 
 console.log("1. say hello");
@@ -16,11 +21,33 @@ rl.question("Enter your choice (1-3)"), choice =>{
         console.log("hello There");
     }else if (choice==="2"){
         console.log("factorial");  
+
+        rl.question("Enter a Number for Factorial:", (numStr) =>{
+            let num = parseInt(numStr);
+           
+            if(isNaN(num) || num<0){
+                console.log("Please Enter a Non-Negative Integer");
+            }else {
+
+                let fact = 1;
+
+                for (Let i = 1; i <= num ; i++){
+                    fact *= i;
+
+                }
+
+                console.log("Factorial of " + num + " is " + fact);
+            }
+
+
+        });
+
+
+     
     }else if (choice==="3"){
         console.log("exit");
     }else{
         console.log("invalid choice. Please Try Again");
 
     }
-
 }
